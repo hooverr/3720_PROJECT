d1b888e16f71c8a2aec5358cca10415098156e46
Robert Hoover <robhoover@shaw.ca>
2013-02-20 12:47:41 -0700

    Testing json feeds

--- feed.php
+++ feed.php
@@ -1,20 +1,47 @@
 <?php
 	
-	$year = date('Y');
-	$month = date('m');
+	$year = date('Y'); //date format is full eg 2013
+	$month = date('m'); //month format is numerical eg 02 for february
 	
 	
+	//Each day is an array
+	//ID is a unique identifier for the doctor
 	echo json_encode(array(
 	
 		array(
+			'id' => 1,
 			'title' => "DoctorA",
 			'contact' => "xxx-xxx-xxxx",
 			'start' => "$year-$month-01",	
+		),
+		
+		array(
+		
+			'id' => 1,
 			'title' => "DoctorA",
 			'contact' => "xxx-xxx-xxxx",
-			'start' => "$year-$month-02",	
+			'start' => "$year-$month-02",
+		
 		),
+		
 		array(
+			'id' => 1,
+			'title' => "DoctorA",
+			'contact' => "xxx-xxx-xxxx",
+			'start' => "$year-$month-03",
+		
+		)
+		
+		array(
+			'id' => 1,
+			'title' => "DoctorA",
+			'contact' => "xxx-xxx-xxxx",
+			'start' => "$year-$month-01",	
+		),
+		
+		array(
+		
+			'id' => 2,
 			'title' => "DoctorB",
 			'contact' => "xxx-xxx-xxxx",
 			'start' => "$year-$month-04",
@@ -22,11 +49,53 @@
 		),
 		
 		array(
+			'id' => 3,
 			'title' => "DoctorC",
 			'contact' => "xxx-xxx-xxxx",
 			'start' => "$year-$month-05",
 		
 		)
+		
+		array(
+		
+			'id' => 1,
+			'title' => "DoctorA",
+			'contact' => "xxx-xxx-xxxx",
+			'start' => "$year-$month-06",
+		
+		),
+		
+		array(
+			'id' => 2,
+			'title' => "DoctorB",
+			'contact' => "xxx-xxx-xxxx",
+			'start' => "$year-$month-07",
+		
+		)
+		
+		array(
+			'id' => 3,
+			'title' => "DoctorC",
+			'contact' => "xxx-xxx-xxxx",
+			'start' => "$year-$month-08",
+		
+		)
+		array(
+			'id' => 3,
+			'title' => "DoctorC",
+			'contact' => "xxx-xxx-xxxx",
+			'start' => "$year-$month-09",
+		
+		)
+		array(
+			'id' => 3,
+			'title' => "DoctorC",
+			'contact' => "xxx-xxx-xxxx",
+			'start' => "$year-$month-10",
+		
+		)
+		
+		
 
 	));
 ?>
