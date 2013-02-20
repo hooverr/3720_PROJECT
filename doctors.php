4a594097c00d2d3b9ea5bc226c8ade37e5ebeb46
Robert Hoover <robhoover@shaw.ca>
2013-02-20 13:12:28 -0700

    Added files for other pages

---
+++ doctors.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html> 
+<html>
+	<head>
+		<title>CPSC 3720 Calls Project</title>
+		<!--
+		 This is the main css file for the project.
+		 -->
+		<link rel="stylesheet" type="text/css" href="mainStyle.css">
+	</head>
+	<body>
+		<header>
+			<h1>Calls Project</h1>
+		</header>
+		<div id="navigation">
+			<a href="index.html">Schedule</a>
+			<a href="doctors.php">Doctors</a>
+			<a href="requests.php">Requests</a>
+			<a href="reports.php">Reports</a>
+		</div>
+		<div id="content">
+			<div id="calendar"></div>
+		</div>
+		<footer></footer>
+	</body>
+</html>
