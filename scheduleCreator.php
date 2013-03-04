d3eed4c712e0dbc5630c1600c8144b2bb78a9af9
Robert Hoover <robhoover@shaw.ca>
2013-03-03 20:46:29 -0700

    Revert back to previous scheduleCreator

--- scheduleCreator.php
+++ scheduleCreator.php
@@ -47,39 +47,37 @@ class ScheduleCreator{
 
 
 
-//if(isset($_REQUEST['data'])){
+if(isset($_REQUEST['data'])){
   $class = new ScheduleCreator();
-  $testArray= array(1,2,3,3,3,6,7,8);
-  //json_decode($_REQUEST['data']);
-  $username = "robh_user";
-	$password = "3720project";
+  $testArray= json_decode($_REQUEST['data']);
+  //array(1,2,3,3,3,6,7,8);
   $temp = $class->createScheduleArray($testArray,02,2013);
 
   //encode the array to be put into the database
   $insert = json_encode($temp);
-  
-  $link = mysql_connect("localhost",$username,$password);
-	if (!$link) {
-		 die('Could not connect: ' . mysql_error());
-	}
-			
-	mysql_select_db("robh_3720",$link);
+  $mysqli = new mysqli('localhost','robh_user','3720project','robh_3720');
 
-  $query = "INSERT INTO schedule VALUES (2, 20,'test')";
-  mysql_query($query);
+  if ($mysqli->connect_error) {
+      die('Connect Error (' . $mysqli->connect_errno . ') '
+              . $mysqli->connect_error);
+  }
+
+  $query = "INSERT INTO schedule VALUES (2, 20,'$insert')";
+  $mysqli->query($query);
+
+
+
 
   $query2 = "SELECT Schedule FROM schedule WHERE Month = 2 AND Year = 20";
 
-  $result = mysql_query($query2);
+  $result = $mysqli->query($query2);
 
-  $row = fetch_array_assoc($result);
+  $row = $result->fetch_array(MYSQL_ASSOC);
 
   $decode = json_decode($row["Schedule"]);
 
   //not needed just a test to see if you can return it back to a proper php array
   echo var_dump($decode);
-  
-  mysql_free_result($result);
-	mysql_close($link);
- // }
+  $mysqli->close();
+  }
 ?>
