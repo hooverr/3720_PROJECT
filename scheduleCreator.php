51ec94247cbd03e3d47033874ff77c7641e03dfe
Robert Hoover <robhoover@shaw.ca>
2013-03-03 20:06:18 -0700

    Changed to non mysqli

--- scheduleCreator.php
+++ scheduleCreator.php
@@ -56,30 +56,32 @@ if(isset($_REQUEST['data'])){
 
   //encode the array to be put into the database
   $insert = json_encode($temp);
-  $mysqli = new mysqli('localhost','robh_user','3720project','robh_3720');
+  
+  $link = mysql_connect("localhost",$username,$password);
+	if (!$link) {
+		 die('Could not connect: ' . mysql_error());
+	}
+			
+	mysql_select_db("robh_3720",$link);
 
-  if ($mysqli->connect_error) {
-      die('Connect Error (' . $mysqli->connect_errno . ') '
-              . $mysqli->connect_error);
-  }
-
-  $query = "INSERT INTO schedule VALUES (2, 20,'test')";
-  $mysqli->query($query);
+  $query = "INSERT INTO schedule VALUES (2, 20,'$insert')";
+  mysql_query($query);
 
 
   
 
   $query2 = "SELECT Schedule FROM schedule WHERE Month = 2 AND Year = 20";
 
-  $result = $mysqli->query($query2);
+  $result = mysql_query($query2);
 
-  $row = $result->fetch_array(MYSQL_ASSOC);
+  $row = fetch_array_assoc($result);
 
   $decode = json_decode($row["Schedule"]);
 
   //not needed just a test to see if you can return it back to a proper php array
   echo var_dump($decode);
   
-  $mysqli->close();
+  mysql_free_result($result);
+	mysql_close($link);
   }
 ?>
