76f70f93600a3fbfe3f81a17a61db6b54327da29
Michael Rowland <michaelr87@gmail.com>
2013-03-13 11:45:51 -0600

    generate.php update again

--- generate.php
+++ generate.php
@@ -79,9 +79,9 @@
 		month = JSON.stringify(month);
 		year = JSON.stringify(year);
 		schedule = JSON.stringify(schedule);
-		$.post('../writeSched.php', { 'month': month });
-		$.post('../writeSched.php', { 'year': year });
-		$.post('../writeSched.php', { 'schedule': schedule });
+		$.post('writeSched.php', { 'month': month });
+		$.post('writeSched.php', { 'year': year });
+		$.post('writeSched.php', { 'schedule': schedule });
 		
 	}
 	
