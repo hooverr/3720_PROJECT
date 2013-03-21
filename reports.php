513c8a19d009532753f6b4028678d7f16b627d3d
Crynux <stephenfgp@gmail.com>
2013-03-20 22:10:24 -0600

    link

--- reports.php
+++ reports.php
@@ -1,7 +1,18 @@
 <!DOCTYPE html> 
 <html>
-
+	<head>
+		<script type="text/javascript">
+	
+			
+		
+		</script>
+	</head>
 	<body>
+		<center>
+			<form id = "RequestForm">
+				<a href="action/report.php">Report Base</a>			
+			</form>
+		</center>
 
 	</body>
 </html>
