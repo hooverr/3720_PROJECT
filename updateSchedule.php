2d5ee31bbf0321ce30521df86b93b37454c347ed
Robert Hoover <robhoover@shaw.ca>
2013-03-23 11:23:11 -0700

    Finish update work
    Added the decrement and increment functions

--- updateSchedule.php
+++ updateSchedule.php
@@ -7,22 +7,35 @@ if(isset($_REQUEST['newDoctor']) && isset($_REQUEST['oldDoctor']) && isset($_REQ
   $day = $_REQUEST['day'];
   
   //figure out what kind of day it is
-  //@todo figure out holidays
   $date = date("D",strtotime("$month/$day/$year"));
   
   if($date = 'Sat' || $date = 'Sun'){
-    $type = "weekend";
+    $type = "Weekend";
   }else{
-    $type = "weekday";
+    $type = "Weekday";
+  }
+  
+  include("holidayCreator.php");
+  $holidayCreator = new HolidayCreator();
+  $holidayArray = $holidayCreator->dateArray($year,NULL); 
+  foreach($holidayArray as $holiday){
+      //A holiday that falls on a weekend, increments weekend not holiday. Holiday incremented on the monday.
+      if(("$holiday" == "$year-$month-$day") && ($type != "Weekend"){
+        $type = "Holiday";
+      }
   }
   
   $mysqli = new mysqli('localhost','robh_user','3720project','robh_3720');
+  //set the new doctor to the schedule
   $query = "UPDATE Schedule SET `$day` = $newDoctor WHERE `Month` = $month AND `Year` = $year";
 	$mysqli->query($query);
+  //decrement old doctors history
+  $query = "UPDATE Doctor_History SET `$type`= `$type`-1 WHERE `Doctor_Id` = $oldDoctor";
+  $mysqli->query($query);
+  //increment new doctors history
+  $query = "UPDATE Doctor_History SET `$type`=`$type`+1 WHERE `Doctor_Id` = $newDoctor";
+  $mysqli->query($query);
   $mysqli->close();
-  
-  $fp = fopen('data.txt', 'w');
-  fwrite($fp,"$query");
   
 }
 ?>
