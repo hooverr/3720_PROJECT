20aa025fe231c47d2470dc3ce3d01cc6f5cdf49d
Rob Hoover <robhoover@shaw.ca>
2013-04-13 20:12:33 -0600

    Clean
    Cleaned up

--- backupSatMar23/holidayFeed.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once('holidayCreator.php');
-$holidayCreator = new HolidayCreator();
-$year = date("Y");
-echo $holidayCreator->jsonString($year);
-?>
--- backupSatMar23/scheduleFeed.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('scheduleCreator.php');
-$scheduleCreator = new ScheduleCreator();
-echo $scheduleCreator->jsonString();
-?>
