f0068f8395462eab27429d30b0f76898271bbbd6
Crynux <stephenfgp@gmail.com>
2013-04-17 11:26:44 -0600

    Added Prev columns

--- doctorStats.php
+++ doctorStats.php
@@ -1,5 +1,5 @@
 <table style="width:100%; text-align: center;" class="ui-widget-content ui-corner-all">
-	<tr style="border-style:solid; font-size: small;" class="ui-widget-header ui-corner-all"><th>Name</th><th>Start Date</th><th>End Date</th><th>Weekdays</th><th>Weekends</th><th>Holidays</th></tr>
+	<tr style="border-style:solid; font-size: small;" class="ui-widget-header ui-corner-all"><th>Name</th><th>Start Date</th><th>End Date</th><th>Weekdays</th><th>Weekends</th><th>Holidays</th><th>Previous Weekdays</th><th>Previous Weekends</th><th>Previous Holidays</th></tr>
 	<?php
 		
 		include('login.php');	
@@ -12,14 +12,14 @@
 
 		mysql_select_db($database,$link);
 
-		$result = mysql_query('SELECT `Name`,`Start_Date`,`End_Date`,`Weekday`,`Weekend`,`Holiday`,`Doctor`.`Doctor_ID` from Doctor_History, Doctor WHERE Doctor_History.Doctor_ID = Doctor.Doctor_ID');
+		$result = mysql_query('SELECT `Name`,`Start_Date`,`End_Date`,`Weekday`,`Weekend`,`Holiday`,`Previous_Weekday`,`Previous_Weekend`,`Previous_Holiday`,`Doctor`.`Doctor_ID` from Doctor_History, Doctor WHERE Doctor_History.Doctor_ID = Doctor.Doctor_ID');
 		if (!$result) {
 			die('Invalid query: ' . mysql_error());
 		}
 
 		while ($row = mysql_fetch_assoc($result)) {
 			$color = $colors[intval($row['Doctor_ID']) % count($colors)];
-			echo "<tr><td style=\"background-color: #eeeeee; text-align: left; border:2px solid ".$color.";\">".$row['Name']."</td><td style=\"border-bottom:1px solid #dddddd;\">".$row['Start_Date']."</td><td  style=\"background-color: #eeeeee;  border-bottom:1px solid #dddddd;\">".$row['End_Date']."&nbsp</td><td style=\"border-bottom:1px solid #dddddd;\">".$row['Weekday']."</td><td style=\"background-color: #eeeeee;  border-bottom:1px solid #dddddd;\">".$row['Weekend']."</td><td style=\"border-bottom:1px solid #dddddd;\">".$row['Holiday']."</td><td style=\"border-bottom:1px solid #dddddd;\">"."</td></tr>";
+			echo "<tr><td style=\"background-color: #eeeeee; text-align: left; border:2px solid ".$color.";\">".$row['Name']."</td><td style=\"border-bottom:1px solid #dddddd;\">".$row['Start_Date']."</td><td  style=\"background-color: #eeeeee;  border-bottom:1px solid #dddddd;\">".$row['End_Date']."&nbsp</td><td style=\"border-bottom:1px solid #dddddd;\">".$row['Weekday']."</td><td style=\"background-color: #eeeeee;  border-bottom:1px solid #dddddd;\">".$row['Weekend']."</td><td style=\"border-bottom:1px solid #dddddd;\">".$row['Holiday']."</td><td style=\"background-color: #eeeeee;  border-bottom:1px solid #dddddd;\">".$row['Previous_Weekday']."</td><td style=\"border-bottom:1px solid #dddddd;\">".$row['Previous_Weekend']."</td><td style=\"background-color: #eeeeee;  border-bottom:1px solid #dddddd;\">".$row['Previous_Holiday']."</td></tr>";
 		}
 
 		mysql_free_result($result);
